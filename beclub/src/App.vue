eb35cba52e0e9e2babba8e8d9dbf6ec57abd9471
Ryan Chen <ryanchen979@gmail.com>
2024-01-15 21:17:07 -0500

    contact us page

--- beclub/src/App.vue
+++ beclub/src/App.vue
@@ -1,39 +1,36 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+        <a href="discord.gg/" target="_blank">Discord</a>
       </nav>
     </div>
   </header>
 
   <RouterView />
+
+  <footer>footer</footer>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  margin-bottom: 2rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+footer {
+  bottom: 0%;
+  width: 100%;
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 20px;
   text-align: center;
   margin-top: 2rem;
 }
@@ -54,32 +51,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
---
+++ beclub/src/views/ContactView.vue
@@ -0,0 +1,433 @@
+<template>
+  <div class="container">
+    <h1>Feel free to reach out to our fellow officers via the methods below</h1>
+
+    <div class="row">
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Ms. Nichols</h2>
+              <p>Club Advisor</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Ms. Nichols</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>YiFan Zheng</h2>
+              <p>President</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>YiFan Zheng</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>yiz@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>keeps</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Leo Yang</h2>
+              <p>Vice President</p>
+            </div>
+          </div>
+
+          <div class="back">
+            <h2>Leo Yang</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>leoy12@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>Twilight</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Jason Shi</h2>
+              <p>Vice President</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Jason Shi</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>dishwasher24</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Jack Shen</h2>
+              <p>Vice President</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Jack Shen</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Jonathan Liu</h2>
+              <p>Secretary</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Jonathan Liu</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Sharon OuYang</h2>
+              <p>Outreach Secretary</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Sharon OuYang</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>sharon</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Ryan Chen</h2>
+              <p>Webmaster</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Ryan Chen</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>ryanc377@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>.xosos.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Danny Chen</h2>
+              <p>WebMaster</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Danny Chen</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>MeritOfDragos</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Jayden Yu</h2>
+              <p>Event Organizer</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Jayden Zheng</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Johnny Chen</h2>
+              <p>Event Organizer</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Johnny Chen</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Alice Lin</h2>
+              <p>Social Media Manager</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Alice Lin</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Anthony Zheng</h2>
+              <p>Officer</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Anthony Zheng</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p>hasira</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="o">
+        <div class="card">
+          <div class="front">
+            <img src="" class="half" />
+            <div class="half">
+              <h2>Ali Malik</h2>
+              <p>Officer</p>
+            </div>
+          </div>
+          <div class="back">
+            <h2>Ali Malik</h2>
+            <div class="backrow">
+              <div class="half">
+                <h3>Email</h3>
+                <p>@nycstudents.net</p>
+              </div>
+              <div class="half">
+                <h3>Discord</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  padding: 5rem;
+  text-align: center;
+  color: white;
+}
+.o {
+  width: 30%;
+  height: 10rem;
+  margin: 0.75rem;
+  border: 1px solid black;
+  border-radius: 25px;
+  background-color: transparent;
+  perspective: 1000px;
+}
+.card {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: 0.5rem;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+.o:hover .card {
+  transform: rotateY(-180deg);
+}
+.front,
+.back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 25px;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+}
+.front {
+  background-color: blue;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: row;
+}
+.back {
+  transform: rotateY(180deg);
+  background-color: blue;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+.backrow {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+.half {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h2 {
+  font-weight: 400;
+}
+h3 {
+  font-weight: 400;
+}
+</style>
